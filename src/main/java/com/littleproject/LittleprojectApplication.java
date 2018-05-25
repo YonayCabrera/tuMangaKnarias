ebd9445fc1b1392c340d990df32875a3efe656d7
YonayCabrera <yonaycl@gmail.com>
2018-05-25 10:52:36 +0100

    initial commit

---
+++ src/main/java/com/littleproject/LittleprojectApplication.java
@@ -0,0 +1,12 @@
+package com.littleproject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LittleprojectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LittleprojectApplication.class, args);
+	}
+}
